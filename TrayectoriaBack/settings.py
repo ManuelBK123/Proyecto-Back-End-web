329eb41954dcc441c92f2d0352821acfab65147e
manuel2001bk <manuel2001bk@gmail.com>
2020-11-05 21:31:22 -0600

    add postgresql and profile

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -103,13 +103,22 @@ WSGI_APPLICATION = 'TrayectoriaBack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'trayectoriadb',
+        'USER': 'postgres',
+        'PASSWORD':'123456789',
+        'HOST': '127.0.0.1',
+        'PORT':'5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
