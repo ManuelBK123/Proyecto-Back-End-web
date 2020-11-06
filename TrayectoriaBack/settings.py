755e057445d24751d6714ca6a12ec632bf8507bd
manuel2001bk <manuel2001bk@gmail.com>
2020-11-06 01:19:30 -0600

    correccion y limpieza de codigo extra

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,27 +98,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'TrayectoriaBack.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-#DATABASES = {
- #   'default': {
- #       'ENGINE': 'django.db.backends.postgresql_psycopg2',
- #       'NAME': 'trayectoriadb',
-#        'USER': 'postgres',
-#        'PASSWORD':'123456789',
-#        'HOST': '127.0.0.1',
-#        'PORT':'5432'
-#    }
-#}
 
 DATABASES = {
     'default': {
